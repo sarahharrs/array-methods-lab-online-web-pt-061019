ad02c0176f6a2fa7e195c5183036cb33711dd51f
Sarah Harrs <sarahharrs@gmail.com>
2019-06-26 16:55:06 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
-def using_include(array, element)
-
+def using_include(array_wedding,"ring")
+  array_wedding = ["flowers", "ring", "champagne"]
+array_wedding.include?("ring")
 end
 
-def using_sort(array)
-
+def using_sort(array_work)
+  array_work = ["laptop", "notepad", "outlook"]
+array_work.sort
 end
 
-def using_reverse(array)
-
+def using_reverse(array_candy)
+array_candy = ["starburst", "blowpops", "chocolate"]
+array_candy.reverse
 end
 
-def using_first(array)
-
+def using_first(array_chips)
+array_chips = ["sun chips", "doritos", "cheetos"]
+array_chips.first
 end
 
-def using_last(array)
-
+def using_last(array_languages)
+array_languages = ["ruby", "java", "HTML", "CSS"]
+array_languages.last
 end
 
-def using_size(array)
-
+def using_size(array_size)
+array_size = [ "big", "small", "medium"]
+array_size.size
 end
